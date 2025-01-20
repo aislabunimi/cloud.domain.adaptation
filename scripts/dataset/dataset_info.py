b76827b58b00746d28975971bd81dfe7b6aefc50
antonazzi <micheleantonazzi@gmail.com>
2025-01-20 10:14:02 +0100

    added file

--- scripts/dataset/dataset_info.py
+++ scripts/dataset/dataset_info.py
@@ -1,5 +1,5 @@
 import os
-from paths import da
+
 test_sem_file = os.path.join(dataset_path, 'splits', 'nvs_sem_train.txt')
 test_semantic = []
 with open(test_sem_file) as file:
---
+++ scripts/paths.py
@@ -0,0 +1,1 @@
+dataset_path = '/home/antonazzi/myfiles/scannetpp'
