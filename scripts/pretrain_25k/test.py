d47c69ff3648394b20ef11f814196c7374ea8384
antonazzi <micheleantonazzi@gmail.com>
2025-01-29 17:58:35 +0100

    added scripts

--- scripts/pretrain_25k/test.py
+++ scripts/pretrain_25k/test.py
@@ -1,11 +1,12 @@
 import os
 import shutil
 from pathlib import Path
-
 import torch
+from pytorch_lightning.strategies import DDPStrategy
 import yaml
+from data_loaders.scannet.pretrain_data_module import DataModule25K
 from models.semantic_segmentator import DeepLabV3Lightning
-from pytorch_lightning import seed_everything
+from pytorch_lightning import seed_everything, Trainer
 from utils.loading import load_yaml
 from utils.paths import REPO_ROOT, RESULTS_PATH
 
@@ -20,10 +21,39 @@ if parameters["general"]["clean_up_folder_if_exists"]:
 
 Path(experiment_path).mkdir(parents=True, exist_ok=True)
 
+####################################
+# Load Model
+###################################
+
 model = DeepLabV3Lightning(parameters)
 
 # Restore pre-trained model
 if parameters['model']['load_checkpoint']:
-    checkpoint = torch.load(os.path.join(RESULTS_PATH, parameters["model"]["checkpoint_path"]), weights_only=True)
-    print(checkpoint)
-#model.load_state_dict(checkpoint, strict=True)
+    checkpoint = torch.load(os.path.join(RESULTS_PATH, parameters["model"]["checkpoint_path"]))
+    checkpoint = checkpoint["state_dict"]
+    # remove any aux classifier stuff
+    removekeys = [
+        key for key in checkpoint.keys()
+        if key.startswith("_model._model.aux_classifier")
+    ]
+    print(removekeys)
+    for key in removekeys:
+        del checkpoint[key]
+    try:
+        model.load_state_dict(checkpoint, strict=True)
+    except RuntimeError as e:
+        print(e)
+    #model.load_state_dict(checkpoint, strict=False)
+
+####################################
+# Load dataset
+###################################
+
+datamodule = DataModule25K(parameters["data_module"])
+
+trainer = Trainer(**parameters["trainer"],
+                  default_root_dir=experiment_path,
+                  strategy=DDPStrategy(find_unused_parameters=False)
+)
+print(type(model))
+trainer.test(model, datamodule=datamodule)
--- utils/paths.py
+++ utils/paths.py
@@ -2,4 +2,4 @@ import os
 
 REPO_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATASET_PATH = '/media/antonazzi/5F35DAE402E93C3F/scannet'
-RESULTS_PATH = '/media/antonazzi/5F35DAE402E93C3F/adaptation_results'
+RESULTS_PATH = '/media/antonazzi/Elements/adaptation_results'
